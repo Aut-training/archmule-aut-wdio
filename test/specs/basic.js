46552d3b693c31844d546a45aaaba6a9635c90bd
Rene Cossio <wilson.cossio.m@gmail.com>
2019-12-13 16:47:08 -0400

    basic login logout implemented

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -7,29 +7,29 @@ const assert = require('assert');
 let should = require('chai').should();
 
 
-describe('Navigate to archmule page', () => {
-  it('should have the right title', () => {
+describe('Login and logout of the archmule page', () => {
+  it('should have the right user and password credentials', () => {
     browser.url('https://archmule.com/login');
 
     // let loginButton = $("a[class='button login']");
     // loginButton.click();
-        
+
     let mailUserTxt = $('#identifier');
     mailUserTxt.setValue("sirius_779@hotmail.com");
-    
+
     let passwrdTxt = $('#password');
     passwrdTxt.setValue('bootcamp');
-    
+
     let loginButton = $("button[type='submit']");
     loginButton.click();
     browser.pause(2000);
-    
+
     let locator = "img[alt=\"user's avatar\"]";
     let userAvatar = $(locator);
     userAvatar.click();
     browser.pause(2000);
-    
-    let logOutBtn = $("button[type='submit']")
+
+    let logOutBtn = $("button[type='submit']");
     logOutBtn.click();
     const url = browser.getUrl();
     url.should.be.equal('https://archmule.com/');
