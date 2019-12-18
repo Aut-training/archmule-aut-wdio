c7d3d779daf6eaaef1961721c39b244134b996b0
Rene Cossio <wilson.cossio.m@gmail.com>
2019-12-18 10:59:54 -0400

    working on delete projectUI

--- test/e2e/pages/Dashboard.page.js
+++ test/e2e/pages/Dashboard.page.js
@@ -3,35 +3,35 @@ class DashboardPage {
     browser.url(path);
   }
 
-  get projectListSidebar() {
-    return $('.ProjectListSidebar__trigger');
-  }
-  
-  get createProjectSide() {
-    return $('.CreateProject');
-  }
+  get projectListSidebar() { return $('.ProjectListSidebar__trigger'); }
 
-  get createProjectBtn() {
-    return $('button>svg.space-right');
-  }
+  get createProjectSide() { return $('.CreateProject'); }
 
-  get projectTittleTxt() {
-    return $('.form-control');
-  }
+  get createProjectBtn() { return $('button>svg.space-right'); }
 
-  get createNewProjectBtn() {
-    return $('span.mx-auto');
-  }
-  
-  get projectTileLabel() {
-    return $('.ProjectInfo__title');
-  }
-  
-  clickProjectSidebar() {    
-    this.projectListSidebar.waitForExist(3000);
+  get projectTittleTxt() { return $('.form-control'); }
+
+  get createNewProjectBtn() { return $('span.mx-auto'); }
+
+  get projectTileLabel() { return $('.ProjectInfo__title'); }
+
+  get projectTitleInSidebar() { return $('div=Project100'); }
+
+  get projectMenu() { return $('.Icon--vertical'); }
+
+  get projectMenuBarMore() { return $('.ProjectSettings__links-more>a'); }
+
+  get projectMoreSettings() { return $('div=Settings'); }
+
+  get projectSettingsDelete() { return $('a[href$="delete"]'); }
+
+  get deleteProjectBtn() { return $('.Button--red'); }
+
+  clickProjectSidebar() {
+    this.projectListSidebar.waitForExist(5000);
     this.projectListSidebar.click();
   }
-  
+
   clickcreateProjectSide() {
     this.createProjectSide.waitForExist(3000);
     this.createProjectSide.click();
@@ -50,9 +50,37 @@ class DashboardPage {
     this.createNewProjectBtn.click();
   }
 
-  getProjectTitleText(){
+  getProjectTitleText() {
     this.projectTileLabel.waitForExist(3000);
     return this.projectTileLabel.getText();
+  }
+
+  clickOnProject() {
+    this.projectTitleInSidebar.click();
+  }
+
+  clickOnProjectMenu() {
+    this.projectMenu.click();
+  }
+
+  clickOnProjectMoreOpt() {
+    this.projectMenuBarMore.click();
+  }
+
+  clickOnProjectMoreSettings() {
+    this.projectMoreSettings.click();
+  }
+
+  clickOnProjectSettingsDelete() {
+    this.projectSettingsDelete.click();
+  }
+
+  clickOnDeleteProjectBtn() {
+    this.deleteProjectBtn.click();
+  }
+
+  acceptAlertOption(){
+    browser.acceptAlert();
   }
 
 }
--- test/e2e/specs/createProject.spec.js
+++ test/e2e/specs/createProject.spec.js
@@ -4,22 +4,58 @@ const context = require('../../data/context');
 const { expect } = require('chai');
 
 describe('Create a new project ', () => {
-  it('should succesfully create a new project', () => {
+  // it('should succesfully create a new project', () => {
+  //   //Login
+  //   loginPage.open('https://archmule.com/login');
+  //   loginPage.fillMailUser(context.logins.user.login);
+  //   loginPage.fillPasswUser(context.logins.user.password);
+  //   loginPage.clickLoginBtn();
+
+  //   //Create Project
+  //   dashboardPage.clickProjectSidebar();
+  //   dashboardPage.clickcreateProjectSide();
+  //   dashboardPage.fillProjectTitle('Project100');
+  //   dashboardPage.clickCreateNewProjectBtn();
+  //   browser.pause(2000);
+
+  //   let result = dashboardPage.getProjectTitleText();
+  //   expect(result).to.equal('Project100');
+  // });
+  
+  it('should delete a project', () => {
     //Login
     loginPage.open('https://archmule.com/login');
     loginPage.fillMailUser(context.logins.user.login);
     loginPage.fillPasswUser(context.logins.user.password);
     loginPage.clickLoginBtn();
 
-    //Create Project
+    //Delete Project
     dashboardPage.clickProjectSidebar();
-    dashboardPage.clickcreateProjectSide();
-    dashboardPage.fillProjectTitle('Project100');
-    dashboardPage.clickCreateNewProjectBtn();
+    browser.pause(1000);
+    dashboardPage.clickOnProject();
+    browser.pause(1000);
+    dashboardPage.clickOnProjectMenu();
     browser.pause(2000);
+    dashboardPage.clickOnProjectMoreOpt();
+    browser.pause(2000);
+    dashboardPage.clickOnProjectMoreSettings();
+    browser.pause(2000);
+    dashboardPage.clickOnProjectSettingsDelete();
+    dashboardPage.clickOnDeleteProjectBtn();
+    browser.acceptAlert();
+    // dashboardPage.acceptAlertOption();
+    
+    // dashboardPage.clickcreateProjectSide();
+    // dashboardPage.fillProjectTitle('Project100');
+    // dashboardPage.clickCreateNewProjectBtn();
+    // browser.pause(2000);
 
-    let result = dashboardPage.getProjectTitleText();
-    expect(result).to.equal('Project100');
+    // let result = dashboardPage.getProjectTitleText();
+    // expect(result).to.equal('Project100');
   });
+
+
+
+  
 
 });
