1945f41d8f378ee4478258d1d9a0c082a089e6c8
Rene Cossio <wilson.cossio.m@gmail.com>
2019-12-19 11:16:47 -0400

    feature create a task implemented

--- test/e2e/pages/Project.page.js
+++ test/e2e/pages/Project.page.js
@@ -1,19 +1,19 @@
 class ProjectPage {
-
+ 
+  get addTaskTextbox() { return $('.AddTask__input'); }
+  
+  get addTaskButton() { return $('.AddTask__submit'); }
+  
+  get taskCard() { return $('span=TaskEX'); }
+  
+  get taskColumnNow() { return $('h3=Now'); }
+  
+  get taskCardColumn() { return $('span=Next'); }
+ 
   open() {
     browser.url('https://archmule.com/@renemendoza/project600');
   }
-
-  get addTaskTextbox() { return $('.AddTask__input'); }
-
-  get addTaskButton() { return $('.AddTask__submit'); }
-
-  get taskCard() { return $('span=TaskEX'); }
-
-  get taskColumnNow() { return $('h3=Now'); }
-
-  get taskCardColumn() { return $('span=Next'); }
-
+  
   fillTaskTextbx(taskTitle) {
     this.addTaskTextbox.waitForExist(3000);
     this.addTaskTextbox.setValue(taskTitle);
@@ -30,7 +30,6 @@ class ProjectPage {
   getTaskCardColumnText() {
     return this.taskCardColumn.getText();
   }
-
 }
 
 module.exports = new ProjectPage();
